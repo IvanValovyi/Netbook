e11ca8c6332f501828d4589f4c43c67d4c326269
TimaVol <tima.voloshuk@gmail.com>
2023-03-07 18:05:00 +0200

    WIP

---
+++ components/Members.tsx
@@ -0,0 +1,16 @@
+export default function Members() {
+  return (
+    <div className="container py-[30px] md:pt-[130px] md:pb-[84px]">
+      <div className="mb-[30px] text-center md:mb-[64px]">
+        <h5 className="mb-[10px] font-medium text-darkBlue">Valuable Team</h5>
+        <h2 className="mb-[19px] font-bold text-black-300">
+          Our Active Members
+        </h2>
+        <p className="mx-auto max-w-[421px] text-lightGray">
+          when an unknown printer took a galley of type and meeting fari
+          scrambled it.
+        </p>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import AdMobApps from "@/components/AdMobApps";
 import Community from "@/components/Community";
+import Members from "@/components/Members";
 import News from "@/components/News";
 
 export default function Home() {
   return (
     <>
       <Community />
+      <Members />
       <AdMobApps />
       <News />
     </>
