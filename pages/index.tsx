9ade6232265ae9c63929123ec0c4f671cb97bf65
TimaVol <tima.voloshuk@gmail.com>
2023-03-07 15:52:36 +0200

    done with Community

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import AdMobApps from "@/components/AdMobApps";
+import Community from "@/components/Community";
 
 export default function Home() {
   return (
     <>
+      <Community />
       <AdMobApps />
     </>
   );
