70fc1a0fbcbe1ce607801d75fb9ff3043dc62d4d
TimaVol <tima.voloshuk@gmail.com>
2023-03-07 16:25:42 +0200

    WIP

---
+++ components/News.tsx
@@ -0,0 +1,10 @@
+export default function News() {
+  return (
+    <div className="container">
+      <h5 className="mb-[10px] font-medium text-darkBlue">
+        Get Our Aplication
+      </h5>
+      <h2 className="mb-[19px] font-bold text-black-300">Latest News</h2>
+    </div>
+  );
+}
